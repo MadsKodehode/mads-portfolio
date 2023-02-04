57fece45531b7784075cf6743f8e2d22d1a07126
MadsKodehode <mads.kodehode@gmail.com>
2023-02-04 22:13:10 +0100

    Responsive on all mobile devices

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,18 +3,21 @@ import MadsImg from "../imgs/MadsiTrappen.JPG";
 const About = () => {
   return (
     <section className="about">
-      <h1 className="about-header">{data.om.header}</h1>
-      <div className="img-wrapper">
-        <img className="about-img" src={MadsImg} alt="mads"></img>
-      </div>
-      <div className="about-btns">
-        <button className="contact">KONTAKT</button>
-        <button className="readmore">LES MER</button>
-      </div>
-      <div className="text-wrapper">
-        <h5 className="about-subheader">{data.om.subheader}</h5>
-        <p className="about-body">{data.om.body}</p>
-        <p className="about-footer">{data.om.footer}</p>
+      <div className="about-container">
+        <h1 className="about-header">{data.om.header}</h1>
+
+        <div className="img-wrapper">
+          <img className="about-img" src={MadsImg} alt="mads"></img>
+        </div>
+        <div className="about-btns">
+          <button className="contact">KONTAKT</button>
+          <button className="readmore">LES MER</button>
+        </div>
+        <div className="text-wrapper">
+          <h5 className="about-subheader">{data.om.subheader}</h5>
+          <p className="about-body">{data.om.body}</p>
+          <p className="about-footer">{data.om.footer}</p>
+        </div>
       </div>
     </section>
   );
