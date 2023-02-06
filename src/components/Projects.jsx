38b955942167ec959c1e27628401fb6a61435486
MadsKodehode <mads.kodehode@gmail.com>
2023-02-06 11:05:03 +0100

    Added styles for larger screens

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,23 +7,32 @@ const Projects = () => {
     <>
       <section className="projects">
         <div className="projects-container">
-          <h1 className="project-header">{data.projekter.header}</h1>
-          <div className="category-wrap">
-            <button className="project-btn">
-              <IoMdArrowDropleft></IoMdArrowDropleft>
-            </button>
-            <h2 className="category">{data.projekter.subheader1}</h2>
-            <button className="project-btn">
-              <IoMdArrowDropright></IoMdArrowDropright>
-            </button>
+          <div className="projects-left">
+            <h1 className="project-header">{data.projekter.header}</h1>
+            <ul className="category-list">
+              <li className="category-item">{data.projekter.subheader1}</li>
+              <li className="category-item">{data.projekter.subheader2}</li>
+              <li className="category-item">{data.projekter.subheader3}</li>
+            </ul>
+            <div className="category-wrap">
+              <button className="project-btn">
+                <IoMdArrowDropleft></IoMdArrowDropleft>
+              </button>
+              <h2 className="category">{data.projekter.subheader1}</h2>
+              <button className="project-btn">
+                <IoMdArrowDropright></IoMdArrowDropright>
+              </button>
+            </div>
           </div>
-          <div className="project">
-            <h3 className="project-title">{data.projekter.title}</h3>
-            <img className="project-img" src={PokedexImg} alt="pokedex"></img>
-            <p className="project-desc">{data.projekter.desc}</p>
-          </div>
-          <div className="project-arrow">
-            <HiOutlineArrowNarrowDown></HiOutlineArrowNarrowDown>
+          <div className="projects-right">
+            <div className="project">
+              <h3 className="project-title">{data.projekter.title}</h3>
+              <img className="project-img" src={PokedexImg} alt="pokedex"></img>
+              <p className="project-desc">{data.projekter.desc}</p>
+            </div>
+            <div className="project-arrow">
+              <HiOutlineArrowNarrowDown></HiOutlineArrowNarrowDown>
+            </div>
           </div>
         </div>
       </section>
