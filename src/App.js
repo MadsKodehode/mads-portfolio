8c3a708ac51d7fa4527444a3ffbd5b50598a4875
MadsKodehode <mads.kodehode@gmail.com>
2023-02-09 23:05:41 +0100

    Version one done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import HomePage from "./components/HomePage";
-import LanguageExp from "./components/LanguagePage/LanguageExp";
-import Projects from "./components/ProjectsPage/Projects";
-import About from "./components/AboutPage/About";
-import Footer from "./components/Footer/Footer";
+
 import Contact from "./components/Contact/Contact";
+
 import "./styles/main.scss";
 import { useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Outlet } from "react-router-dom";
 
 function App() {
   return (
